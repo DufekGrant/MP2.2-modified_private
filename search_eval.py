d4e994cff8b308a85f00ca17303b74d411682ce9
DufekGrant <117484162+DufekGrant@users.noreply.github.com>
2023-10-11 19:57:01 -0700

    Add files via upload

--- search_eval.py
+++ search_eval.py
@@ -11,9 +11,9 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index.
     """
-    return(metapy.index.OkapiBM25(1.4,1,5))
+    #return(metapy.index.OkapiBM25(1.4,1,5))
     #return(metapy.index.DirichletPrior())
-    #return(metapy.index.AbsoluteDiscount(fitted))
+    return(metapy.index.AbsoluteDiscount(0.5))
     #return(metapy.index.PivotedLength())
     #return(metapy.index.JelinekMercer())
     #return metapy.index.OkapiBM25(k1=1.2,b=0.75,k3=500)
